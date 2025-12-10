e780238f4c8be2d5f2d74250a76ba8bef67df5b0
DESKTOP-NR6TVAI\LAPTOP <amiraelsaftytraining@gmail.com>
2025-12-10 16:41:07 +0200

    + Added appbar .

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -7,6 +7,9 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.teal,
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+      ),
     );
   }
 }
